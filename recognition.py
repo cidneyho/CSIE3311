ec6209ddf08ae8417d6f4d7a2f01df761ceb781e
cyd <yhoad@connect.ust.hk>
2020-10-18 17:55:59 +0800

    update recognition

--- recognition.py
+++ recognition.py
@@ -1,36 +1,18 @@
 import cv2
 import numpy as np
 
-def knn_training(img):
-	img = cv2.imread(img)
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	
-	divide = list(np.hsplit(i,100) for i in np.vsplit(gray, 50))
-	array = np.array(divide)
-	
-	train_data = array[:, :50].reshape(-1,400).astype(np.float32)
-	
-	k = np.arange(10)
-	
-	train_labels = np.repeat(k, 250)[:, np.newaxis]
-	
-	knn = cv2.ml.KNearest_create()
-	knn.train(train_data, cv2.ml.ROW_SAMPLE, train_labels)
-	
-	return knn
-
 def recognize(img, knn):
-	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	img = cv2.resize(img, (20, 20), cv2.INTER_LINEAR)
-	img = img.flatten()
-	img = img.astype(np.float32)
+    if img is None: return '?'
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (28, 28), cv2.INTER_LINEAR)
+    img = img.flatten()
+    img = img.astype(np.float32)
 	
-	L = [img]
-	data = np.array(L)
-	ret, output, neighbours, distance = knn.findNearest(data, k = 3)
-	return int(output[0][0])
-	
+    data = np.array([img])
+    ret, output, neighbours, distance = knn.findNearest(data,k = 3)
+    return int(output[0][0])
+
 if __name__ == '__main__':
-	knn = knn_training('training.png')
-	img = cv2.imread('test.png')
-	print(recognize(img, knn))
+    knn = cv2.ml.KNearest_load('knn')
+    img = cv2.imread('test.png')
+    print(recognize(img, knn))
